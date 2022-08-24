7f56655e20cc519018b971844cb151f1547c3157
Cessarrb <cesarstopdc@gmail.com>
2022-08-23 21:33:27 -0300

    up

---
+++ 01 - Máximo mínimo y promedio/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace _01___Máximo_mínimo_y_promedio
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numeroIngresado = 0;
+            int numMaximo = int.MinValue;
+            int numMinimo = int.MaxValue;
+            int sumaNumIngresados = 0;
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Ingrese un numero: ");
+
+                if(int.TryParse(Console.ReadLine(), out numeroIngresado)){
+
+                    if(numeroIngresado > numMaximo)
+                    {
+                        numMaximo = numeroIngresado;
+                    }
+                    if(numeroIngresado < numMinimo)
+                    {
+                        numMinimo = numeroIngresado;
+                    }
+
+                    sumaNumIngresados += numeroIngresado;
+                }
+                else
+                {
+                    Console.WriteLine("Debe ingresar solo numeros");
+                }
+            }
+
+            Console.WriteLine($"Numero Minimo {numMinimo} -- numero maximo {numMaximo} -- promedio {(float)sumaNumIngresados / 5}");
+            
+        }
+    }
+}
